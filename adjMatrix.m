2c56e35a2f8e883773642731b97fa01aeeaaeaa1
Slewy <fw22616@bristol.ac.uk>
2023-11-20 14:49:12 +0000

    adj for multiplex network

--- adjMatrix.m
+++ adjMatrix.m
@@ -1,16 +1,22 @@
 % Intra-layer adjacency matrices
-adjMatrixLayer1 = [0 1 1; 
-                   1 0 1; 
-                   1 1 0]; % Layer 1 adjacency matrix
-adjMatrixLayer2 = [0 1 0; 
-                   1 0 1;
-                   0 1 0]; % Layer 2 adjacency matrix
+adjMatrixLayer1 = [0 1 0 0 1; 
+                   1 0 1 0 1; 
+                   0 1 0 0 0;
+                   0 0 0 0 0;
+                   1 1 0 0 0]; % Layer 1 adjacency matrix
+adjMatrixLayer2 = [0 1 0 0 0 ; 
+                   1 0 1 0 0;
+                   0 1 0 1 0;
+                   0 0 1 0 0;
+                   0 1 0 0 0]; % Layer 2 adjacency matrix
 intraLayerAdjMatrices = {adjMatrixLayer1, adjMatrixLayer2};
 
 % Inter-layer adjacency matrices
-interLayerAdjMatrix1 = [0 1 0; 
-                        0 0 0; 
-                        0 0 0]; % Connections between nodes of Layer 1 and Layer 2
+interLayerAdjMatrix1 = [0 1 1 1 0; 
+                        1 0 0 0 0;
+                        1 0 0 1 0;
+                        1 0 1 0 0;
+                        0 0 0 0 0]; % Connections between nodes of Layer 1 and Layer 2
 interLayerAdjMatrices = {interLayerAdjMatrix1};
 
 % Flattening the multiplex network
