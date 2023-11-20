932a8e1907760b20bc14d159a8d68a244a7a1784
Slewy <fw22616@bristol.ac.uk>
2023-11-20 14:39:09 +0000

    adj multiplex network

---
+++ adjMatrix.m
@@ -0,0 +1,20 @@
+% Intra-layer adjacency matrices
+adjMatrixLayer1 = [0 1 1; 
+                   1 0 1; 
+                   1 1 0]; % Layer 1 adjacency matrix
+adjMatrixLayer2 = [0 1 0; 
+                   1 0 1;
+                   0 1 0]; % Layer 2 adjacency matrix
+intraLayerAdjMatrices = {adjMatrixLayer1, adjMatrixLayer2};
+
+% Inter-layer adjacency matrices
+interLayerAdjMatrix1 = [0 1 0; 
+                        0 0 0; 
+                        0 0 0]; % Connections between nodes of Layer 1 and Layer 2
+interLayerAdjMatrices = {interLayerAdjMatrix1};
+
+% Flattening the multiplex network
+flattenedNetwork = flatten(intraLayerAdjMatrices, interLayerAdjMatrices);
+
+% Display the flattened network's adjacency matrix
+disp(flattenedNetwork);
